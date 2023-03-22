8a0e12f4d73d1cd2db69a44e36cd7fa475a04c84
bashward <nikhilt626@gmail.com>
2023-03-22 12:46:33 +0530

    bug fixes

--- Stream/app.js
+++ Stream/app.js
@@ -426,7 +426,7 @@ function addInfo(fileId, fileInfo){
   var info = {id: fileId, info: fileInfo}
   info.getVideoLength = new Promise((resolve, reject) => {
     if(!info.videoLength){
-      getDuration('https:localhost:9001' + '/' + fileId).then((duration) => {
+      getDuration('https://nikflix-stream.vercel.app' + '/' + fileId).then((duration) => {
         info.videoLength = duration
         resolve(duration)
       })
